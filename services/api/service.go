033259dd47ce50bd9ceebf308459198383ea41ed
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2024-09-13 11:42:25 +0200

    feat(service): log where payload was found

--- services/api/service.go
+++ services/api/service.go
@@ -1348,6 +1348,7 @@ func (api *RelayAPI) checkProposerSignature(block *common.VersionedSignedBlinded
 func getPayloadWithFallbacks(log *logrus.Entry, datastoreRef *datastore.Datastore, slot uint64, proposerPubkey common.PubkeyHex, blockHash phase0.Hash32) (*builderApi.VersionedSubmitBlindedBlockResponse, error) {
 	getPayloadResp, err := datastoreRef.LocalPayloadContents(uint64(slot), proposerPubkey.String(), blockHash.String())
 	if getPayloadResp != nil {
+		log.Info("payload found locally")
 		return getPayloadResp, nil
 	}
 
@@ -1363,6 +1364,7 @@ func getPayloadWithFallbacks(log *logrus.Entry, datastoreRef *datastore.Datastor
 	// Check there.
 	getPayloadResp, err = datastoreRef.RedisPayload(log, uint64(slot), proposerPubkey.String(), blockHash.String())
 	if getPayloadResp != nil {
+		log.Info("payload found in redis")
 		return getPayloadResp, nil
 	}
 
@@ -1379,6 +1381,7 @@ func getPayloadWithFallbacks(log *logrus.Entry, datastoreRef *datastore.Datastor
 	// Regardless, its possible the other geo does. Check it, so we may help out in publishing, even if it wasn't "our bid".
 	getPayloadResp, err = datastoreRef.RemotePayloadContents(uint64(slot), proposerPubkey.String(), blockHash.String())
 	if getPayloadResp != nil {
+		log.Info("payload found remotely")
 		return getPayloadResp, nil
 	}
 
